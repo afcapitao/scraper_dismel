821e47dbd0240b55f8fb61a65a7c3db96c46ebf8
André Capitão <andre.f.g.capitao@gmail.com>
2023-11-02 21:54:58 +0000

    Remove not needed tags

--- common.py
+++ common.py
@@ -11,17 +11,23 @@ def extractDescricao(soup, isFormatted):
   for section_div in divs: 
     content_text_div = section_div.find('div', class_='content-text')
     if content_text_div:
-        return content_text_div.get_text(strip=True) if isFormatted=='false' else content_text_div
+      if isFormatted == 'true':
+        remove_tag(content_text_div,'div')
+        remove_tag(content_text_div,'span')
+        remove_tag(content_text_div,'a')
+        remove_tag(content_text_div,'input')
+      return content_text_div.get_text(strip=True) if isFormatted=='false' else content_text_div
 
 def extractProductName(soup):
   row_div = soup.find('div', class_='ba-row row-fluid ba-no-gutter rooms-row')
   if row_div:
       return row_div.get_text(strip=True)
 
-def remove_style_attributes(soup):
+def remove_attributes(soup,attr):
+  if soup is not None:
     for tag in soup.find_all(True):
-        if tag.has_attr("style"):
-            del tag["style"]
+        if tag.has_attr(attr):
+            del tag[attr]
 
 def filter_phrases(text):
   text=text.replace("\n","")
@@ -38,9 +44,19 @@ def extractAllUrls(soup):
               hrefs.append(href)
   return hrefs
 
-def remove_tag(soup, tagName):
-  for tag in soup.find_all(tagName):
-    tag.unwrap()
+def remove_tag(element, tagName, htmlClass=''):
+  if element is not None:
+    elements=element.find_all(tagName) if htmlClass=='' else element.find_all(tagName, class_=htmlClass)
+    for tag in element.find_all(tagName):
+      tag.unwrap()
+  return element
+
+def removeTagsNotNeeded(element):
+  remove_tag(element,'div')
+  remove_tag(element,'span')
+  remove_tag(element,'a')
+  remove_tag(element,'input')
+  remove_tag(element,'img')
 
 def buildFileName(filename, isFormatted):
   return Constants.EXTRACTION_FOLDER+filename+('' if isFormatted=='false' else Constants.FORMATTED_TEXT) + Constants.EXTENSION
@@ -53,5 +69,6 @@ class Constants:
   FILENAME_SENS_DISP_MOV="sensores_dispositivos_moveis"
   FILENAME_SENSORES="sensores"
   FILENAME_SOFTWARE="software"
+  FILENAME_KITS="kits_ciencias"
   FORMATTED_TEXT="_formatted"
   EXTENSION=".xlsx"
